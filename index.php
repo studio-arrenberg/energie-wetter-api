36c64f7f29f4087d742b4becbacf23f740b66275
j0hannr <johann.rohn@gmail.com>
2021-05-25 10:24:39 +0200

    API tweaks

--- index.php
+++ index.php
@@ -6,48 +6,49 @@
 // mysql connection
 require_once 'config_mysql.php';
 
+// mysql charset
 mysqli_set_charset($mysqli, "utf8");
 
 // init array
 $data = [];
 
 // general information
-$general = array('general' => array('description' => 'hello World', 'source' => 'The API consists of data from Wuppertaler Stadtwerke. If you would like to add you data please contact hallo@arrenberg.studio.', 'maintained' => 'Studio Arrenberg maintains this API for the usage in the Wuppertal Region.'));
+$general = array('description' => 'The Energie Wetter is displaying the C02 production for current supplied energy in Wuppertal.', 'source' => 'The API consists of data from Wuppertaler Stadtwerke. If you would like to add you data please contact hallo@arrenberg.studio.', 'maintained' => 'Studio Arrenberg maintains this API for the usage in the Wuppertal Region.');
 
 // query now
 $now = date('Y-m-d H').":00";
 // $now = '2019-05-29 17:00';
 
 $query_now = "SELECT ampel_status.color as color, FLOOR( RAND() * (( ampel_status.carbon_factor + 10) - (ampel_status.carbon_factor - 10)) + (ampel_status.carbon_factor - 10)) as gramm  FROM `Ampel` 
-join ampel_status on Ampel.status = ampel_status.id
-WHERE `timestamp` = '$now'
-Limit 0,1";
+  join ampel_status on Ampel.status = ampel_status.id
+  WHERE `timestamp` = '$now'
+  Limit 0,1";
 
 $result = mysqli_fetch_array(mysqli_query($mysqli, $query_now));
 $current = array('color' => $result['color'], 'emissions' => array('amount' => $result['gramm'], 'unit' => 'g C02 / KWh'), 'lable' => array('singular' =>  $result['color'], 'plural' =>  $result['color']));
   
 // query forcast
 $query_forecast = "SELECT
-ampel_status.color,
-ampel_status.name,
-DATE_FORMAT(Ampel.timestamp, '%H:%i') AS time,
-unix_timestamp(Ampel.timestamp) AS DATE
-FROM
-Ampel
-JOIN ampel_status ON Ampel.status = ampel_status.id
-WHERE
-`timestamp` BETWEEN '".$now."' AND(
-    '".$now."' + INTERVAL 48 HOUR
-)
-order by Ampel.timestamp asc
-LIMIT 0, 60";
+  ampel_status.color,
+  ampel_status.name,
+  DATE_FORMAT(Ampel.timestamp, '%H:%i') AS time,
+  unix_timestamp(Ampel.timestamp) AS DATE
+  FROM
+  Ampel
+  JOIN ampel_status ON Ampel.status = ampel_status.id
+  WHERE
+  `timestamp` BETWEEN '".$now."' AND(
+      '".$now."' + INTERVAL 48 HOUR
+  )
+  order by Ampel.timestamp asc
+  LIMIT 0, 60";
 
 $forecast = [];
 
 $timeline_r = mysqli_query($mysqli, $query_forecast) or die("could not perform query");
 while($row = mysqli_fetch_array($timeline_r)) {
 
-  $forecast[$row['DATE']] = array('color' => $row['color'], 'lable' => $row['name'], 'time' => $row['time'], 'timestamp' => $row['DATE'] );
+  $forecast[$row['DATE']] = array('color' => $row['color'], 'lable' => $row['name'], 'time' => $row['time'] );
 
 }
 
@@ -69,8 +70,3 @@ $data = array('general' => $general, 'current' => $current, 'forecast' => $forec
 // display data as API in JSON format
 header('Content-Type: application/json');
 echo json_encode($data, true);
-  
-
-
-?>
-
